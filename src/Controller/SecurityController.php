b0fc5c585825fff3c663f9924afd01a2fdacd98e
N1Code7 <n1code7@outlook.fr>
2022-08-01 11:38:14 +0200

    feat: add registration function + modify logout route in security.yaml

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    #[Route(path: "/inscription", name: "app_security_registration")]
+    public function registration(Request $request, UserRepository $repository): Response
+    {
+        $user = new User();
 
+        $form = $this->createForm(RegistrationType::class, $user);
 
-    #[Route(path: '/login', name: 'app_login')]
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $repository->add($user, true);
+
+            return $this->redirectToRoute("app_security_login", [
+                "user" => $user
+            ]);
+        }
+
+        return $this->render("security/registration.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    #[Route(path: '/connexion', name: 'app_security_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -26,7 +51,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/deconnexion', name: 'app_security_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
