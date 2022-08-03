08a2a679e32a4cf3ae1ec86625760535b4129a45
N1Code7 <n1code7@outlook.fr>
2022-08-03 11:34:13 +0200

    :sparkles: add pizzas list on homepage

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\PizzaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_front_home_index')]
+    public function index(PizzaRepository $repository): Response
+    {
+        $pizzas = $repository->findAll();
+
+        return $this->render('front/home/index.html.twig', [
+            'pizzas' => $pizzas
+        ]);
+    }
+}
